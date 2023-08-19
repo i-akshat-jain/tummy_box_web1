be40afd49dcce1c72975c86e3813896bdb2ea634
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2023-08-19 20:32:23 +0100

    data fetched properly commit 4

--- lib/home/homeView.dart
+++ lib/home/homeView.dart
@@ -43,91 +43,97 @@ class _HomeViewState extends State<HomeView> {
 
           final userDocs = snapshot.data!.docs;
 
-          return Row(
-            children: [
-              Expanded(
-                
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.builder(
-                      itemCount: userDocs.length,
-                      itemBuilder: (context, index) {
-                        final displayName =
-                            userDocs[index]['display_name'] ?? 'No Name';
-                        final userReferenceId = userDocs[index].id;
-                        return ListTile(
-                          title: Text(displayName),
-                          onTap: () async {
-                            DocumentSnapshot userSnapshot =
-                                await FirebaseFirestore.instance
-                                    .collection('Users')
-                                    .doc(userReferenceId)
-                                    .get();
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      color: Colors.blue,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ListView.builder(
+                          itemCount: userDocs.length,
+                          itemBuilder: (context, index) {
+                            final displayName =
+                                userDocs[index]['display_name'] ?? 'No Name';
+                            final userReferenceId = userDocs[index].id;
+                            return ListTile(
+                              title: Text(displayName),
+                              onTap: () async {
+                                DocumentSnapshot userSnapshot =
+                                    await FirebaseFirestore.instance
+                                        .collection('Users')
+                                        .doc(userReferenceId)
+                                        .get();
 
-                            DocumentReference userDocRef =
-                                userSnapshot.reference;
-                            CollectionReference profilesCollection =
-                                userDocRef.collection("profiles");
+                                DocumentReference userDocRef =
+                                    userSnapshot.reference;
+                                CollectionReference profilesCollection =
+                                    userDocRef.collection("profiles");
 
-                            QuerySnapshot profilesSnapshot =
-                                await profilesCollection.get();
+                                QuerySnapshot profilesSnapshot =
+                                    await profilesCollection.get();
 
-                            List<Map<String, dynamic>> profiles = [];
+                                List<Map<String, dynamic>> profiles = [];
 
-                            profilesSnapshot.docs.forEach((profileDoc) {
-                              Map<String, dynamic> profileData =
-                                  profileDoc.data() as Map<String, dynamic>;
-                              profiles.add(profileData);
-                            });
+                                profilesSnapshot.docs.forEach((profileDoc) {
+                                  Map<String, dynamic> profileData =
+                                      profileDoc.data()
+                                          as Map<String, dynamic>;
+                                  profiles.add(profileData);
+                                });
 
+                                setState(() {
+                                  selectedUserData = userSnapshot.data()
+                                      as Map<String, dynamic>;
+                                  profilesData = profiles;
+                                  showUserDetails = true;
+                                  showProfileDetails = false;
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (showUserDetails)
+                    Expanded(
+                      child: Container(
+                        color: Colors.yellow,
+                        height: MediaQuery.of(context).size.height * 0.92,
+                        width: double.infinity,
+                        child: UserDetailsScreen(
+                          userData: selectedUserData,
+                          profilesData: profilesData,
+                          showProfileDetails: showProfileDetails,
+                          setshowProfileDetails: (bool value,
+                              Map<String, dynamic> selectprofileData) {
                             setState(() {
-                              selectedUserData =
-                                  userSnapshot.data() as Map<String, dynamic>;
-                              profilesData = profiles;
-                              showUserDetails = true;
-                              showProfileDetails = false;
+                              selectedProfileData = selectprofileData;
+                              showProfileDetails = value;
                             });
                           },
-                        );
-                      },
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                  if (showProfileDetails)
+                    Expanded(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.92,
+                        width: double.infinity,
+                        color: Colors.green,
+                        child: ProfileScreen(
+                          profileData: selectedProfileData,
+                        ),
+                      ),
+                    ),
+                ],
               ),
-              if (showUserDetails)
-                Expanded(
-                  child: Container(
-                    color: Colors.yellow,
-                    height: MediaQuery.of(context).size.height * 0.92,
-                    width: double.infinity,
-                    child: UserDetailsScreen(
-                      userData: selectedUserData,
-                      profilesData: profilesData,
-                      showProfileDetails: showProfileDetails,
-                      setshowProfileDetails: (bool value,  profileData) {
-                        setState(() {
-                          selectedProfileData = profilesData[0];
-                          showProfileDetails = value;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              if (showProfileDetails)
-                Expanded(
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.92,
-                    width: double.infinity,
-                    color: Colors.green,
-                    child: ProfileScreen(
-                      profileData: selectedProfileData,
-                    ),
-                  ),
-                ),
-            ],
+            ),
           );
         },
       ),
@@ -159,37 +165,52 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Name: ${widget.userData['display_name']}'),
-          Text('Email: ${widget.userData['email']}'),
-          Text('Phone Number: ${widget.userData['phone_number']}'),
-          // Display other user details as needed
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'User Details',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle edit button press
+                    // You can navigate to an edit screen or show a dialog
+                  },
+                  child: Text('Edit'),
+                ),
+              ],
+            ),
+            Text('Name: ${widget.userData['display_name']}'),
+            Text('Email: ${widget.userData['email']}'),
+            Text('Phone Number: ${widget.userData['phone_number']}'),
+            // Display other user details as needed
 
-          SizedBox(height: 20),
+            SizedBox(height: 20),
 
-          Text(
-            'Profiles:',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Column(
-            children: widget.profilesData.map((profile) {
-              return ListTile(
-                title: Text('Profile Name: ${profile['name']}'),
-                onTap: () {
-                  setState(() {
-                    selectedProfile = profile;
-                  });
-                  widget.setshowProfileDetails(true, profile);
-                },
-              );
-            }).toList(),
-          ),
-
-          if (selectedProfile != null)
-            ProfileScreen(profileData: selectedProfile!),
-        ],
+            Text(
+              'Profiles:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              children: widget.profilesData.map((profile) {
+                return ListTile(
+                  title: Text('Profile Name: ${profile['name']}'),
+                  onTap: () {
+                    setState(() {
+                      selectedProfile = profile;
+                    });
+                    widget.setshowProfileDetails(true, profile);
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -209,14 +230,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Name: ${widget.profileData['name']}'),
-          Text('Date of Birth: ${widget.profileData['dob']}'),
-          Text('Gender: ${widget.profileData['gender']}'),
-          // Display other profile details
-        ],
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Profile Details',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle edit button press
+                    // You can navigate to an edit screen or show a dialog
+                  },
+                  child: Text('Edit'),
+                ),
+              ],
+            ),
+            Text('Name: ${widget.profileData['name']}'),
+            Text('Date of Birth: ${widget.profileData['dob']}'),
+            Text('Gender: ${widget.profileData['gender']}'),
+            // Display other profile details
+          ],
+        ),
       ),
     );
   }
