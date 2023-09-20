4318b36e0c942e8ec891f38630b8d8dd445e82e6
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2023-09-20 09:32:09 +0100

    Errored

--- lib/menu/menu_view.dart
+++ lib/menu/menu_view.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -41,88 +42,97 @@ class _MenuPageState extends State<MenuPage> {
 
           final menuDocs = snapshot.data!.docs;
           return Container(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListView.builder(
-                      itemCount: subcollectionNames.length,
-                      itemBuilder: (context, index) {
-                        final subcollectionName = subcollectionNames[index];
-                        return ListTile(
-                          title: Text(subcollectionName),
-                          onTap: () {
-                            setState(() {
-                              showMealData = true;
-                            });
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      color: Colors.blue,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ListView.builder(
+                          itemCount: subcollectionNames.length,
+                          itemBuilder: (context, index) {
+                            final subcollectionName = subcollectionNames[index];
+                            return ListTile(
+                              title: Text(subcollectionName),
+                              onTap: () {
+                                setState(() {
+                                  showMealData = true;
+                                });
+                              },
+                            );
                           },
-                        );
-                      },
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              if (showMealData)
-                Expanded(
-                  child: Container(
-                    color: Colors.yellow,
-                    width: double.infinity,
-                    child: ListView.builder(
-                      itemCount: subcollectionNames.length,
-                      itemBuilder: (context, index) {
-                        final menuReferenceId = menuDocs[index].id;
-                        final subcollectionName = subcollectionNames[index];
-                        return StreamBuilder<QuerySnapshot>(
-                          stream: FirebaseFirestore.instance
-                              .collection('Menu')
-                              .doc(
-                                  menuReferenceId) // Replace with the actual document ID you want to fetch
-                              .collection(subcollectionName)
-                              .snapshots(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return Center(
-                                child: Text('Error: ${snapshot.error}'),
-                              );
-                            }
-                            if (!snapshot.hasData) {
-                              return Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
+                  if (showMealData)
+                    Expanded(
+                      child: Container(
+                        color: Colors.yellow,
+                        width: double.infinity,
+                        child: ListView.builder(
+                          itemCount: subcollectionNames.length,
+                          itemBuilder: (context, index) {
+                            final menuReferenceId = menuDocs[index].id;
+                            final subcollectionName = subcollectionNames[index];
+                            return StreamBuilder<QuerySnapshot>(
+                              stream: FirebaseFirestore.instance
+                                  .collection('Menu')
+                                  .doc(menuReferenceId)
+                                  .collection(subcollectionName)
+                                  .snapshots(),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasError) {
+                                  return Center(
+                                    child: Text('Error: ${snapshot.error}'),
+                                  );
+                                }
+                                if (!snapshot.hasData) {
+                                  return Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
 
-                            final subcollectionDocs = snapshot.data!.docs;
-                            return ListTile(
-                              title: Text(displayName),
-                              onTap: () async {
-                              itemCount: subcollectionDocs.length,
-                              itemBuilder: (context, index) {
-                                final subcollectionItem =
-                                    subcollectionDocs[index].data()
-                                        as Map<String, dynamic>;
-                                return ListTile(
-                                  title: Text(subcollectionItem['item1']),
-                                  subtitle: Text(subcollectionItem['item2']),
-                                  // You can display other subcollection data here
+                                final subcollectionDocs = snapshot.data!.docs;
+
+                                // Create a list of ListTiles for the subcollection data
+                                List<Widget> subcollectionListTiles =
+                                    subcollectionDocs.map((subcollectionItem) {
+                                  if (index < subcollectionDocs.length) {
+                                    final subcollectionData = subcollectionItem
+                                        .data() as Map<String, dynamic>;
+                                    return ListTile(
+                                      title: Text(subcollectionData['item1']),
+                                      subtitle:
+                                          Text(subcollectionData['item2']),
+                                      // You can display other subcollection data here
+                                    );
+                                  } else {
+                                    return ListTile(
+                                      title: Text("No Data"),
+                                      subtitle: Text("No Data"),
+                                      // You can display other subcollection data here
+                                    );
+                                  }
+                                }).toList();
+
+                                return ListView(
+                                  children: subcollectionListTiles,
                                 );
                               },
                             );
                           },
-                        );
-                      },
-                    ),
-                  ),
-                )
-            ],
-          ),
-        ),
-      );
+                        ),
+                      ),
+                    )
+                ],
+              ),
+            ),
+          );
         },
       ),
     );
